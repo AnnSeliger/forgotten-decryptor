d6d5cca106f6e43f5b54974c339b3c9dac41740e
AnnSeliger <annaseliger@yandex.ru>
2025-04-26 14:13:27 +0300

    fix css import and Tailwind apply

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // <-- подключение стилей!
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // <--- подключение Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
